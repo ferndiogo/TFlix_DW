83da92629a31acc12f67deeb7b53da9533dec8dc
Tomás Silva <aluno23052@ipt.pt>
2022-04-30 16:08:46 +0100

    Criação das classes de relação

---
+++ TFlix/TFlix/Models/Gere.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TFlix.Models
+{
+    public class Gere
+    {
+        public Gere()
+        {
+            Admins = new HashSet<Admin>();
+            Utilizadores = new HashSet<Utilizador>();
+        }
+
+        [ForeignKey("Admin")]
+        public int AdminFK { get; set; }
+        public Admin Admin { get; set; }
+
+        [ForeignKey("Utilizador")]
+        public int UtilizadorFK { get; set; }
+        public Utilizador Utilizador { get; set; }
+
+        public ICollection<Admin> Admins { get; set; }  
+        public ICollection<Utilizador> Utilizadores { get; set; }
+    }
+}
