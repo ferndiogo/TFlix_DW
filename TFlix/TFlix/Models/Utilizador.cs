131aa3fdb1dbcfc73315ac8013d7e5b1427da8e1
Tomás Silva <aluno23052@ipt.pt>
2022-05-10 13:44:08 +0100

    Defesa dos campos de registo

--- TFlix/TFlix/Models/Utilizador.cs
+++ TFlix/TFlix/Models/Utilizador.cs
@@ -1,4 +1,6 @@
-﻿namespace TFlix.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TFlix.Models
 {
     public class Utilizador
     {
@@ -13,14 +15,27 @@
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage ="O {0} é de preenchimento obrigatório.")]
+        [StringLength(30, ErrorMessage ="O {0} não pode ter mais de {1} caractéres.")]
+        [Display(Name = "Nome")]
+        [RegularExpression("[A-ZÂÓÍa-záéíóúàèìòùâêîôûãõäëïöüñç '-]+", ErrorMessage = "Só pode escrever letras no {0}")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage ="O {0} é de preenchimento obrigatório.")]
+        [EmailAddress(ErrorMessage ="Escreva um {0} válido, por favor.")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage ="O {0} é de preenchimento obrigatório.")]
+        [MinLength(8, ErrorMessage ="A {0} tem de ter mais de {1} caractéres.")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "O {0} tem de ter {1} carateres.")]
+        [RegularExpression("[123578][0-9]{8}", ErrorMessage = "O {0} deve começar por 1,2,3,5,7,8 seguido de 8 digitos numéricos.")]
         public string NIF { get; set; }
 
+        [Required(ErrorMessage ="O {0} é de preenchimento obrigatório.")]
+        
         public string Morada { get; set; }
 
         public ICollection<Admin> Administradores { get; set; }
