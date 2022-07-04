c9609b8f4e9721308328e5fa0d577d9d6915e185
Fernando Fuzeiro <aluno22111@ipt.pt>
2022-07-04 01:10:50 +0100

    Alteração no atributo data.

--- TFlix/TFlix/Data/Migrations/20220703235217_AlteracaoData.Designer.cs
+++ TFlix/TFlix/Data/Migrations/20220703235217_AlteracaoData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TFlix.Data;
 
@@ -11,9 +12,10 @@ using TFlix.Data;
 namespace TFlix.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220703235217_AlteracaoData")]
+    partial class AlteracaoData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
